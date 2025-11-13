e9a0dab289b8863735af242df77d355aad93ed1f
YunHak Lee <lyh990408@gmail.com>
2025-11-13 17:33:48 +0900

    [Chore] 코드 정리, 디버그용 함수 수정

--- Sources/FigmaUILabel/FigmaUILabel.swift
+++ Sources/FigmaUILabel/FigmaUILabel.swift
@@ -8,8 +8,7 @@
 import UIKit
 
 public final class FigmaUILabel: UILabel {
-    private var needsTextStyleUpdate = false
-    
+    //MARK: Label Properties
     public override var text: String? { didSet { setNeedsTextStyleUpdate() } }
     public override var font: UIFont! { didSet { setNeedsTextStyleUpdate() } }
     public override var textAlignment: NSTextAlignment { didSet { setNeedsTextStyleUpdate() } }
@@ -21,35 +20,40 @@ public final class FigmaUILabel: UILabel {
     public var kerning: FigmaMetric = .natural { didSet { setNeedsTextStyleUpdate() } }
     public var underlineStyle: NSUnderlineStyle? { didSet { setNeedsTextStyleUpdate() } }
     
+    //MARK: TextStyle Update
+    private var needsTextStyleUpdate = false
+    
     private func setNeedsTextStyleUpdate() {
         guard !needsTextStyleUpdate else { return }
         needsTextStyleUpdate = true
         
         DispatchQueue.main.async { [weak self] in
-            self?.updateTextStyleIfNeeded()
-            self?.needsTextStyleUpdate = false
+            guard let self else { return }
+            self.updateTextStyleIfNeeded()
+            self.needsTextStyleUpdate = false
         }
     }
     
     private func updateTextStyleIfNeeded() {
-        debugPrintUnderline(self, tag: "before builder")
-        
         TextStyleBuilder(self)
             .lineHeight(lineHeight.resolvedValue(for: font))
             .kerning(kerning.resolvedValue(for: font))
             .underlineStyle(underlineStyle)
             .apply()
-        
-        debugPrintUnderline(self, tag: "after builder")
     }
-    private func debugPrintUnderline(_ label: UILabel, tag: String) {
+    
+    func debugUnderlineRuns(_ label: UILabel, tag: String) {
         guard let attr = label.attributedText else {
             print("[\(tag)] no attributedText")
             return
         }
-        let length = attr.length
-        let attrs = attr.attributes(at: 0, effectiveRange: nil)
-        print("[\(tag)] length: \(length), attrs[0]: \(attrs)")
+
+        let fullRange = NSRange(location: 0, length: attr.length)
+        attr.enumerateAttribute(.underlineStyle,
+                                in: fullRange,
+                                options: []) { value, range, _ in
+            print("[\(tag)] underline attr in range \(range): \(String(describing: value))")
+        }
     }
 }
 
